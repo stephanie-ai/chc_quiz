ab599b7f0b88e6864ce2e0679e6d989546a78031
stephanie-ai <stephanie_cooper@hotmail.com>
2021-06-28 08:47:41 +0100

    create header and navbar

---
+++ src/Components/navbar/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './style.css';
+
+const NavBar = () => {
+    return (
+        <nav className="navBar">
+            
+             <NavLink  exact to="/" className="nav" activeClassName="current"> Home</NavLink>
+             {/* <NavLink to="/game" className="nav1" activeClassName="current">Game</NavLink> */}
+        </nav>
+    );
+}
+
+
+export default NavBar;
---
+++ src/layout/header/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from '../../components/navbar';
+// import logo from './logo.png';
+import './style.css';
+
+class Header extends React.Component {
+    render(){
+        return (
+            <div className="header">
+                {/* <img className="logoImg" src={logo}/> */}
+                <h1 className="chcTitle"> C Hoare & Co Quiz </h1>
+                <NavBar />
+            </div>
+        );
+    }
+}
+
+export default Header;
