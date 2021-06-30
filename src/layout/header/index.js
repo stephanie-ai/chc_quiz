565d8f5bfc7900f46dda8af33116a54777a23733
stephanie-ai <stephanie_cooper@hotmail.com>
2021-06-30 15:30:31 +0100

    add logo

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 // import NavBar from '../../components/navbar';
-// import logo from './logo.png';
+import logo from '../../../public/images/white_logo.png';
 import './style.css';
 
 class Header extends React.Component {
     render(){
         return (
             <div className="header">
-                {/* <img className="logoImg" src={logo}/> */}
-                <h1 className="chcTitle"> C Hoare & Co Quiz </h1>
+                <img id="logoImg" src={logo}/>
+                {/* <h1 className="chcTitle"> C Hoare & Co Quiz </h1> */}
                 {/* <NavBar /> */}
+                <div id="headerQuiz">
+                  <h1>Quiz</h1>  
+                </div>
+                
             </div>
         );
     }
